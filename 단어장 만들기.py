274866457fd696ffdb20e64eb709e279d90d455e
swiss0906 <swiss0906@naver.com>
2021-09-22 20:41:22 +0900

    Create 단어장 만들기.py

---
+++ 단어장 만들기.py
@@ -0,0 +1,11 @@
+with open('vocabulary.txt', 'w') as f:
+    while True:
+        english_word = input('영어 단어를 입력하세요: ')
+        if english_word == 'q':
+            break
+
+        korean_word = input('한국어 뜻을 입력하세요: ')
+        if korean_word == 'q':
+            break
+
+        f.write('{}: {}\n'.format(english_word, korean_word))
