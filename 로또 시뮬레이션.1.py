f8a72348145f7800e4cb77bd01042059cf173ed3
swiss0906 <swiss0906@naver.com>
2021-09-23 18:44:15 +0900

    Create 로또 시뮬레이션.1.py

---
+++ 로또 시뮬레이션.1.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+def generate_numbers(n):
+    numbers = []
+
+    while len(numbers) < n:
+        num = randint(1, 45)
+        if num not in numbers:
+            numbers.append(num)
+
+    return numbers
+
+
+# 예시 결과 출력
+print(generate_numbers(6))
